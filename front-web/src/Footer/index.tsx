48507015d60a355e621a9b4877a777ea548156e0
William Florido <william.frdo@gmail.com>
2021-03-28 23:44:35 -0300

    Added Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,26 @@
+import './styles.css';
+import { ReactComponent as YouTubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedInIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            Trabalho de Conclusão de Curso de Desenvolvimento de Sistemas ETEC 2021 < br/>
+            Camila S. M. A. Borges, Christiane N. Gozza, Wesley C. Davanzo, William Florido
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/channel/UCc7rfCbP90qiw3BgT-Iqc7g" target="_new">
+                    <YouTubeIcon />
+                </a>
+                <a href="https://www.linkedin.com/company/governosp/" target="_new">
+                    <LinkedInIcon />
+                </a>
+                <a href="https://www.instagram.com/governosp/" target="_new">
+                    <InstagramIcon />
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
