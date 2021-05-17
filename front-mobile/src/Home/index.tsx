ddf9b52fb13cfeed4497397422fbb98460e67238
William Florido <william.frdo@gmail.com>
2021-05-16 21:44:03 -0300

    Add Orders List

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,30 +1,35 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 function Home() {
-    const handleOnPress = () => {
+  const navigation = useNavigation();
 
-    }
+  const handleOnPress = () => {
+    navigation.navigate('Orders');
+  }
 
-    return (
-        <>
-            <View style={styles.container}>
-                <Image source={require('../assets/deliveryman.png')}/>
-                <Text style={styles.title}>
-                    Acompanhe os pedidos e {'\n'} entregue no prazo!
-                </Text>
-                <Text style={styles.subTitle}>
-                    Receba todos os pedidos do seu {'\n'} restaurante na palma da sua mão
-                </Text>
-            </View>
-            <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={handleOnPress}>
-                    <Text style={styles.buttonText}>VER PEDIDOS</Text>
-                </RectButton>
-            </View>
-        </>
-    );
+  return (
+      <>
+        <Header />
+        <View style={styles.container}>
+            <Image source={require('../assets/deliveryman.png')}/>
+            <Text style={styles.title}>
+                Acompanhe os pedidos e {'\n'} entregue no prazo!
+            </Text>
+            <Text style={styles.subTitle}>
+                Receba todos os pedidos do seu {'\n'} restaurante na palma da sua mão
+            </Text>
+        </View>
+        <View style={styles.footer}>
+            <RectButton style={styles.button} onPress={handleOnPress}>
+                <Text style={styles.buttonText}>VER PEDIDOS</Text>
+            </RectButton>
+        </View>
+      </>
+  );
 }
 
 const styles = StyleSheet.create({
